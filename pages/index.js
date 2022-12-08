ff276d7f51c4bb7a1f8d042e2b9e56d763d2af99
MSCOP <Mikkelschreiner96@gmail.com>
2022-12-08 14:00:02 +0100

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Link from 'next/link';
+import Image from 'next/image';
+
+const YourComponent = () => (
+  <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+);
 
 export default function Home() {
   return (
@@ -11,6 +22,9 @@ export default function Home() {
       </Head>
 
       <main>
+      <h1 className="title">
+  Welcome to <a href="https://nextjs.org">Next.js!</a>
+</h1>
         <Header title="This is my first App" />
         <p className="description">
           And it acutally works. This WebApp is built with next.js
